822a6fccaec650def49e108ff6e8f3b0ff03f5c7
riskanursafitri <safitririska010@gmail.com>
2021-04-05 00:35:16 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.main')
+@section('title', 'Laravel - SI Perpustakaan')
+@section('content')
+
+<div class="container">
+    <div class="jumbotron">
+        <h1 class="display-4">Selamat Datang!</h1>
+        <hr class="my-4">            
+    </div>
+</div>
+
+@endsection
